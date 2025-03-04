531e66ef295a41806ae05ce2a6ecb670dc95c8b8
Servuli <svoverdose@gmail.com>
2025-03-04 18:20:40 +0100

    Initial commit

---
+++ src/Switch.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+// Created by Vadim 04.03.2025
+public class Switch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введи возраст");
+        int age = scanner.nextInt();
+        switch (age){
+            case 0:
+                System.out.println("Ты родился");
+                break;
+            case 7:
+                System.out.println("Ты пошел в школу");
+                break;
+            case 18:
+                System.out.println("Ты закончил школу");
+                break;
+            default:
+                System.out.println("Ни одно из предыдущих условий не подошло");
+        }
+    }
+}
